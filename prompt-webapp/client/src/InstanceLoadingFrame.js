0bbae0f481daa867e80f1e303c134eb8b48b52c2
Derek Bekoe <debekoe@microsoft.com>
2018-01-17 18:38:11 -0800

    UI for instance loading

---
+++ prompt-webapp/client/src/InstanceLoadingFrame.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Stepper, { Step, StepLabel, StepContent } from 'material-ui/Stepper';
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+
+const styles = theme => ({
+  root: {
+    width: '90%',
+  },
+  button: {
+    marginRight: theme.spacing.unit,
+  },
+  actionsContainer: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit * 10,
+  },
+  resetContainer: {
+    marginTop: 0,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+function getSteps() {
+  return ['Creating your instance', 'Waiting for DNS propogation', 'Starting your instance'];
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return `For each pull request, an instance is created for you.`;
+    case 1:
+      return 'The instance has been created. We are setting up a DNS entry.';
+    case 2:
+      return `Starting up your instance. Not long now.`;
+    default:
+      return 'Unknown step';
+  }
+}
+
+class InstanceLoadingFrame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeStep: 0
+    };
+  }
+
+  handleInstanceReady = () => {
+    var instanceSrc = 'https://azure-cli-cd83b165-5808-4629-95c6-469d1d60d2d9-5242-1bda7.instance.prompt.ws:21779/?token=LgKSdG1LMT9SVe5HPRMmRODIVpiE9huBwPuxzvqRkZJ_BeiC7u-WfFFowJlhZE0FX35Z_tAFqVuFuRTgN64RyiZ7XtGbI6W_C85lLAfnFyfCAaAS6FG3DzEp-pQPj6WbxhfO-xFmKectSeWYUIAWIV52pikh6kyaukXGEqBD4lB1i3VWfqEmVBoQJTWl8kaYrcWBFQbQHV66hyA3pTE73Rhzhpmq3d4Xfw9HIrCGBVCVDMNcPAVQw2DxohbWcUPWYwVQJwPz4ElU70xR9udUQsvXzhGDR7w0Xf2OP5vozFn5-Qacx2VYFraTDhyo9C-g1OzMZCz6nw3k1J3A0c5Hmg';
+    this.props.onInstanceReady(instanceSrc);
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+          activeStep: this.state.activeStep + 1,
+      });
+    }, 1000);
+    setTimeout(() => {
+      this.setState({
+          activeStep: this.state.activeStep + 1,
+      });
+    }, 2000);
+    setTimeout(() => {
+      this.setState({
+          activeStep: this.state.activeStep + 1,
+      });
+    }, 3000);
+  }
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
+    return (
+      <div className="InstanceFrame">
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((label, index) => {
+            return (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+                {/* <StepContent>
+                  <Typography>{getStepContent(index)}</Typography>
+                </StepContent> */}
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length && (
+          <Paper square elevation={0} className={classes.resetContainer}>
+            <Typography>Your instance is ready!</Typography>
+            <Button onClick={this.handleInstanceReady} className={classes.button}>
+              Go
+            </Button>
+          </Paper>
+        )}
+      </div>
+    );
+  }
+}
+
+InstanceLoadingFrame.propTypes = {
+  classes: PropTypes.object,
+};
+
+export default withStyles(styles)(InstanceLoadingFrame);
--- prompt-webapp/client/src/PRPage.js
+++ prompt-webapp/client/src/PRPage.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react';
 
 import Button from 'material-ui/Button';
 import PromptAppBar from './PromptAppBar';
+import InstanceLoadingFrame from './InstanceLoadingFrame';
 import InstanceFrame from './InstanceFrame';
 import Footer from './Footer';
+
+class InstanceView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instanceSrc: undefined
+    };
+    this.handleInstanceReady = this.handleInstanceReady.bind(this);
+  }
+
+  handleInstanceReady(instanceSrc) {
+    this.setState({instanceSrc: instanceSrc});
+  }
+
+  render() {
+    const instanceSrc = this.state.instanceSrc;
+    if (instanceSrc) {
+      return <InstanceFrame src={instanceSrc} />
+    }
+    return <InstanceLoadingFrame onInstanceReady={this.handleInstanceReady} />
+  }
+
+}
 
 class PRView extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      instanceSrc: "https://azure-cli-cd83b165-5808-4629-95c6-469d1d60d2d9-5242-1bda7.instance.prompt.ws:21779/?token=LgKSdG1LMT9SVe5HPRMmRODIVpiE9huBwPuxzvqRkZJ_BeiC7u-WfFFowJlhZE0FX35Z_tAFqVuFuRTgN64RyiZ7XtGbI6W_C85lLAfnFyfCAaAS6FG3DzEp-pQPj6WbxhfO-xFmKectSeWYUIAWIV52pikh6kyaukXGEqBD4lB1i3VWfqEmVBoQJTWl8kaYrcWBFQbQHV66hyA3pTE73Rhzhpmq3d4Xfw9HIrCGBVCVDMNcPAVQw2DxohbWcUPWYwVQJwPz4ElU70xR9udUQsvXzhGDR7w0Xf2OP5vozFn5-Qacx2VYFraTDhyo9C-g1OzMZCz6nw3k1J3A0c5Hmg"
-    };
     this.title = this.props.match.params.org + '/' + this.props.match.params.repo + ' PR #' + this.props.match.params.prNum;
   }
 
@@ -23,7 +44,7 @@ class PRView extends Component {
     return (
       <div>
         <PromptAppBar title={this.title} githubSrc={'https://github.com/'+this.props.match.params.org+'/'+this.props.match.params.repo+'/pull/'+this.props.match.params.prNum} />
-        <InstanceFrame src={this.state.instanceSrc} />
+        <InstanceView />
         <Footer />
       </div>
     );
